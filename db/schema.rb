9e73e3bddce3a751a298525228229f550f9863f9
windsorbrown <rahmanzr@gmail.com>
2016-05-11 17:39:29 -0700

    added questions.csv

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511215829) do
-
-  create_table "questions", force: :cascade do |t|
-    t.text     "question"
-    t.text     "answer"
-    t.string   "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
