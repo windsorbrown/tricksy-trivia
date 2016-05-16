a2d3b541c3aca50bce911e61b81d470084f62627
Jason Larigakis <jlarigakis@gmail.com>
2016-05-15 18:31:07 -0700

    Add basic question view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,10 @@
 class QuestionsController < ApplicationController
+  before_action :load_question, only: :show
   
   def show
-    
   end
 
+  def load_question
+    @question = Question.find(params[:id])
+  end
 end
