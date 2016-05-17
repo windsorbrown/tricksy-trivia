09ebb42be3caf13d5c59e35c82ea99ef2a241383
Brett Weinkauf <bweinkau@ualberta.ca>
2016-05-17 19:13:26 +0000

    pull jason master

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-  
+
   def self.create_with_omniauth(auth)
     user = User.find_by(uid: auth['uid'], provider:  auth['provider'])
     if User.exists?(user)
-      user  
+      user
     else
-      user = User.create(uid: auth['uid'], 
+      user = User.create(uid: auth['uid'],
             provider:  auth['provider'], name: auth['info']['name'],
-            image: auth['info']['image'], oauth_token: auth['credentials']['token'], 
+            image: auth['info']['image'], oauth_token: auth['credentials']['token'],
              oauth_secret: auth['credentials']['secret'])
       user.save
       user
@@ -21,7 +21,7 @@ class User < ApplicationRecord
       config.access_token        = oauth_token
       config.access_token_secret = oauth_secret
     end
-    
+
     client.update(tweet)
   end
 
