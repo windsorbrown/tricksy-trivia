196d845f2983a2762e195c031877395a4b899763
Jason Larigakis <jlarigakis@gmail.com>
2016-05-17 17:25:39 -0700

    Add games index

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,10 @@
 class GamesController < ApplicationController
+  def create
+    @user = User.find(session[:user_id])
+    @game = Game.create(owner: @user)
+  end
+  
+  def index
+    render json: Game.all
+  end
 end
