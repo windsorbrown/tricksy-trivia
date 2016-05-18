c89ed1fbccc40e3c32809a30be57f86830906e2b
Jason Larigakis <jlarigakis@gmail.com>
2016-05-17 17:58:11 -0700

    Basic game render

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,7 @@ class GamesController < ApplicationController
   def create
     @user = User.find(session[:user_id])
     @game = Game.create(owner: @user)
+    render :show, layout: 'page'
   end
   
   def index
