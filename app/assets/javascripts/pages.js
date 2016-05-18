3c252b226945c523451d1b729dee76c8f798d8a7
Brett Weinkauf <bweinkau@ualberta.ca>
2016-05-18 21:48:08 +0000

    Merged to my master

--- app/assets/javascripts/pages.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $("#game-form").submit(function(e) {
-        e.preventDefault();
-        let game_number = $(this).find('input[name="game_number"]').val();
-        window.location.href = "/games/" + game_number;
-    });
-});
