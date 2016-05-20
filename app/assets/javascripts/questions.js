e332168858612552b42d529d7703c31da23a4c53
Jason Larigakis <jlarigakis@gmail.com>
2016-05-19 17:49:43 -0700

    Submit question answers

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -8,52 +8,60 @@ function getQuestions() {
   var urlsplit = urlreal.split("/").slice(-1)[0];
   var res = urlreal.replace(urlsplit, "");
   var makeurl = window.location.origin + res + 'play';
-  console.log(makeurl);
    $.ajax({
-      method: 'get',
-       url: makeurl,
-       dataType: 'json',
-       success: function(response) {         
-         response.forEach(function(question) {
-          setTimeout(function(){ showQuestion(question); }, i*2000);
-          i+=1; 
-        });       
-     }
-  });
+     method: 'get',
+     url: makeurl,
+     dataType: 'json',
+     success: startQuestions
+   });
 }
 
-function showQuestion(question){
-  let choiceButton = function(answerText) {
-    return $("<button>")
-      .click(function () {
-        let questionId = $('#game-questions').data('questionId');
-        $.ajax({
-          url: 'user_answers',
-          method: 'post',
-          dataType: 'json',
-          data: {
-            answer: answerText, 
-            question_id: questionId
-          },
-          success: (res) => { alert(res.correct ? "Correct!" : "Wrong!"); }
-        });
-      })
-    .text(answerText);
-  };
+function startQuestions(questions) {         
+  let i = 0;
+  nextQuestion();
+  function nextQuestion() {
+    if (i == questions.length) {
+      alert("finished");
+      return;
+    }
+    let question = questions[i++];
+    let choiceButton = function(answerText) {
+      return $("<button>")
+        .click(function () {
+          let questionId = $('#game-questions').data('questionId');
+          $.ajax({
+            url: 'user_answers',
+            method: 'post',
+            dataType: 'json',
+            data: {
+              answer: answerText, 
+              question_id: questionId
+            },
+            success: (res) => { 
+              alert(res.correct ? "Correct!" : "Wrong!"); 
+              nextQuestion(); 
+              clearTimeout(questionTimeout);
+            }
+          });
+        })
+      .text(answerText);
+    };
 
-  let tr = $("<tr>");
-   tr.append([
-     $("<td>").text(question.category),
-     $("<td>").text(question.question),
-     $("<td>").append(choiceButton(question.choice1)),
-     $("<td>").append(choiceButton(question.answer)),
-     $("<td>").append(choiceButton(question.choice2)),
-     $("<td>").append(choiceButton(question.choice3))
-   ]);
-   $("#game-questions")
-     .empty()
-     .append(tr)
-     .data('questionId', question.id);
+    let tr = $("<tr>");
+    tr.append([
+        $("<td>").text(question.category),
+        $("<td>").text(question.question),
+        $("<td>").append(choiceButton(question.choice1)),
+        $("<td>").append(choiceButton(question.answer)),
+        $("<td>").append(choiceButton(question.choice2)),
+        $("<td>").append(choiceButton(question.choice3))
+    ]);
+    $("#game-questions")
+      .empty()
+      .append(tr)
+      .data('questionId', question.id);
+    let questionTimeout = setTimeout(nextQuestion, 2000);
+  }
 }
 
 
