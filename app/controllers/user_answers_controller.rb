2897acb8520c5e32258a4c2a1aed35e2c7e99998
windsorbrown <rahmanzr@gmail.com>
2016-05-21 19:42:36 -0700

    indentation

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -1,11 +1,13 @@
 class UserAnswersController < ApplicationController
   def create
+    
     @answer = UserAnswer.create(
       user: current_user,
       game: Game.find(params['game_id']),
       answer: params['answer'],
       question: Question.find(params['question_id'])
     )
+    
     ActionCable.server.broadcast "game_#{@answer.game.id}",
     question: {question: @answer.question_id, user:current_user, correct: @answer.correct}
     
