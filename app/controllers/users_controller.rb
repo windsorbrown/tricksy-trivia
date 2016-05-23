a23ee5d5bb3a4552975c197a65670661f286c975
samwoodson <samuelwoodson@gmail.com>
2016-05-23 23:24:20 +0000

    scoreboard updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = @current_user
     @games = Game.where(status: 'pending', keep_private: 0)
     recentscores = UserAnswer.created_between(1.day.ago, Time.now)
-    @topscores = recentscores.group(:user).sum(:score).sort
+    @topscores = recentscores.group(:user).sum(:score)
   end
 
   def create
