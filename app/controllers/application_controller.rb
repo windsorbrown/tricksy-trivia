f8b16422b74357f785c502f90eb5a26300825b20
Jason Larigakis <jlarigakis@gmail.com>
2016-05-23 14:58:00 -0700

    Fix breaking typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  def current_usez
+  def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
