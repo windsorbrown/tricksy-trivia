80739a1937e3ff94de827767c836eece9d5975fd
windsorbrown <rahmanzr@gmail.com>
2016-05-24 15:27:52 -0700

    index page changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
   def index
+     recentscores = UserAnswer.created_between(1.day.ago, Time.now)
+    @topscores = recentscores.group(:user).sum(:score) 
   end
 
   def dash
