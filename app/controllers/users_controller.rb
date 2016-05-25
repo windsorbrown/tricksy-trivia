9db0fa3363fb895f8c77fcdae57dcd419fbc9f65
Jason Larigakis <jlarigakis@gmail.com>
2016-05-24 21:30:12 -0700

    Fix pie chart!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,25 +2,16 @@ class UsersController < ApplicationController
   before_filter :restrict_access
 
   def show
-    @user = @current_user
     @games = Game.where(status: 'pending', keep_private: 0)
 
     recentscores = UserAnswer.created_between(1.day.ago, Time.now)
     @topscores = recentscores.group(:user).sum(:score)
 
-    @win_loss = ActiveRecord::Base.connection.execute %{
-      SELECT (
-        CASE winner WHEN 't' THEN 'Wins' ELSE 'Losses' END
-        ) AS label, count(id) AS Total
-      FROM players WHERE user_id = #{@current_user.id} and winner is not null
-      GROUP BY winner
-      ORDER BY count(id) desc
-    }
-
+    wins = Player.where(user: current_user).where(winner: true).count
+    losses = Player.where(user: current_user).where.not(winner: true).count
+    @win_loss = { wins: wins, losses: losses }
   end
 
   def create
-
   end
-
 end
