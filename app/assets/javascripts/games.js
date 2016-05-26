9aeeb4ae5cca613b9252505bc52994bc058d41ae
samwoodson <samuelwoodson@gmail.com>
2016-05-26 07:50:52 +0000

    removed dupes

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -28,7 +28,6 @@ function subscribeRoomChannel(game_id) {
       var playerList = $('#player-list');
       if (data.added_user) {
         var user = data.added_user;
-        console.log(user);
         playerList.append(`<li id="user-${user.id}"><img src="${user.id.image}" class="img-circle" alt="80" height="50" width="50" /><span class="users-list-name">${user.name}</span></li>`);
       } else if (data.removed_user) {
         var _user = data.removed_user;
